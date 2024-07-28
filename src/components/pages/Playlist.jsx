24c30f19fb97f90ff1a886e29f963f83b10a54ed
Mnyiaa <ntjust26@gmail.com>
2024-07-29 02:35:18 +0900

    fixed api

--- src/components/pages/Playlist.jsx
+++ src/components/pages/Playlist.jsx
@@ -1,19 +1,52 @@
 
 import React from 'react';
 import '../../App.css';
+import '../playlist.css';
 // import styles from '../../styles/Home.module.css'
 
-export default function Playlist() {
-  const thisdata = []
+
+
+function Playlist() {
+
   
-async function getVideos(): Promise<any[]> {
-const apiUrl = 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=25&playlistId=PLg-QWgQ907Hoz61oESylThb2MK9vPC6rk&key=AIzaSyBOg2M8iFPKIJ9_G9hcxUV56yFFa9icglk'
-
-thisdata => fetch(apiUrl)
-.then(data => data.json())
-.then(list => list.items);
+const apiUrl = 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=513&playlistId=PLg-QWgQ907Hoz61oESylThb2MK9vPC6rk&publishedAfter=2019-09-28T00:00:00Z&key=AIzaSyBOg2M8iFPKIJ9_G9hcxUV56yFFa9icglk'
 
 
- console.log(thisdata)
+  fetch(apiUrl)
+.then(res => {
+ return res.json();
+ })
+ .then(data=>{
+  data.items.forEach((curr=>{
+    var vidTitle = curr.snippet.title;
+    var vidUrl = 'https://www.youtube.com/watch?v=' + curr.snippet.resourceId.videoId;
+    var vidThumbnail = curr.snippet.thumbnails.default.url;
+    var date = curr.snippet.publishedAt;
+    // console.log(vidThumbnail, date);
+         
+        var markup = `<div class='videoitem'><a class='vidlink' href='${vidUrl}' target='_blank'><img class='vidimg' src='${vidThumbnail}'></img></a><p>${vidTitle}</p></div>`;
+console.log('counter');
+            document.querySelector('vid').insertAdjacentHTML('beforeend',markup);
+
+              
+
+
+      
+}))
+ })
+
+return (
+  <>
+
+
+<div className='videolist'>
+  <vid className='videoitems'>
+    
+  </vid>
+  </div>
+  </>
+
+  );
 }
-}
+
+export default Playlist;
