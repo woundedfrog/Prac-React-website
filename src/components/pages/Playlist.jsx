6cb9f8e4f3ab9844c60ffc2f00f5d294edcbc27c
Mnyiaa <ntjust26@gmail.com>
2024-07-29 13:30:37 +0900

    api working with issues

--- src/components/pages/Playlist.jsx
+++ src/components/pages/Playlist.jsx
@@ -9,14 +9,21 @@ import '../playlist.css';
 function Playlist() {
 
   
-const apiUrl = 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=513&playlistId=PLg-QWgQ907Hoz61oESylThb2MK9vPC6rk&publishedAfter=2019-09-28T00:00:00Z&key=AIzaSyBOg2M8iFPKIJ9_G9hcxUV56yFFa9icglk'
+// const apiUrl2 = 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLg-QWgQ907Hoz61oESylThb2MK9vPC6rk&key=AIzaSyARvK7mrv4xbrRCwmt05YSu-q5U1UFwqNc'
 
+const apiUrl = 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&pageToken='
+const apiUrlEnd = '&playlistId=PLg-QWgQ907Hoz61oESylThb2MK9vPC6rk&publishedAfter=2019-09-28T00:00:00Z&key=AIzaSyARvK7mrv4xbrRCwmt05YSu-q5U1UFwqNc'
+var nextpage = 'EAAajQFQVDpDRElpRUVSRVJVVkdORVEyTkRjNU1FVTVOVGNvQVVpejZ0LUMxcjJIQTFBQldrUWlRMmxLVVZSSFkzUlZWbVJ1VlZScmQwNHdhSFpsYWxsNFlqQldWR1ZYZUZWaFIwbDVWRlZ6TldSc1FrUk9ia3B5UldkelNURmplbDkwUVZsUmRWQnlVMHhSSWc'
+var url = apiUrl + apiUrlEnd;
+const array = [];
 
-  fetch(apiUrl)
+var count = 0;
+  fetch(url)
 .then(res => {
  return res.json();
  })
  .then(data=>{
+  // console.log(data.items);
   data.items.forEach((curr=>{
     var vidTitle = curr.snippet.title;
     var vidUrl = 'https://www.youtube.com/watch?v=' + curr.snippet.resourceId.videoId;
@@ -25,28 +32,40 @@ const apiUrl = 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=sni
     // console.log(vidThumbnail, date);
          
         var markup = `<div class='videoitem'><a class='vidlink' href='${vidUrl}' target='_blank'><img class='vidimg' src='${vidThumbnail}'></img></a><p>${vidTitle}</p></div>`;
-console.log('counter');
-            document.querySelector('vid').insertAdjacentHTML('beforeend',markup);
+if (array.indexOf(markup) === -1) {
+            
+console.log(array);
 
-              
+array.push(markup);
+
+}
+    count++;  
 
 
-      
+    // console.log(array.indexOf(markup) , array);  
+
 }))
- })
+  
+array.forEach((items) => {
+  console.log(array.indexOf(items) >! 1);
+  if (array.indexOf(items) >= 1) {
+    document.querySelector('vid').insertAdjacentHTML('beforeend',items);
+  };
+}
+ 
+ )
+  })
 
-return (
+
+ return ( 
   <>
 
 
-<div className='videolist'>
   <vid className='videoitems'>
     
   </vid>
-  </div>
   </>
-
-  );
+)
 }
 
 export default Playlist;
