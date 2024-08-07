9507f2850b1c1d24810a42cccec23434d05fd47a
Mnyiaa <ntjust26@gmail.com>
2024-08-07 23:53:03 +0900

    added a search function for the displayed vids

--- src/components/pages/Playlist.jsx
+++ src/components/pages/Playlist.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import '../../App.css';
 import '../playlist.css';
+import axios from 'axios';
 import {
     useRef,
     useState,
     useEffect
 } from "react";
 import xtype from 'xtypejs';
+
 const apiUrl = 'https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50';
 // var playlistId = ''; //'&playlistId=PLg-QWgQ907Hoz61oESylThb2MK9vPC6rk';
 const apiUrlEnd = '&key=AIzaSyBOg2M8iFPKIJ9_G9hcxUV56yFFa9icglk';
-// var nextPage = '';
-// var prevPage = '';
-// const newUrl = '';
+
 let array = [];
 var count = 0;
 // const [vidCount, setCount] = useState(0);
@@ -21,6 +21,7 @@ function Format() { // console.log("next___  " + nextPage);
     const [vidCount, setCount] = useState(0);
     const [newUrl, setNewUrl] = useState('');
     const inputRef = useRef('');
+    const searchVal = useRef('');
     const [playlistId, setPlaylistId] = useState(inputRef.current.value);
     const [nextPage, setNextPage] = useState('');
     const [prevPage, setPrevPage] = useState('');
@@ -30,10 +31,10 @@ function Format() { // console.log("next___  " + nextPage);
 
         setPlaylistId(inputRef.current.value.length == 0 ? '&playlistId=PLg-QWgQ907Hoz61oESylThb2MK9vPC6rk' : '&playlistId=' + inputRef.current.value);
     }
-    
+
     useEffect(() => {
-    (nextPage !== 'NoNewUrl') ? setNewUrl(apiUrl + nextPage + playlistId + apiUrlEnd) : setNewUrl('DeadEnd');
-    }, [playlistId, apiUrl, nextPage, apiUrlEnd]);
+        (nextPage !== 'NoNewUrl') ? setNewUrl(apiUrl + nextPage + playlistId + apiUrlEnd) : setNewUrl('DeadEnd');
+    }, [playlistId, nextPage]);
 
     const getVideosPage = (nn) => {
         console.log();
@@ -42,7 +43,7 @@ function Format() { // console.log("next___  " + nextPage);
             return
         };
         fetch(nn).then(res => {
-          return (!res.ok) ? alert('No valid Url') : response = res.json();
+            return (!res.ok) ? alert('No valid Url') : response = res.json();
 
         }).then(data => {
             if (typeof data == "undefined") {
@@ -53,53 +54,89 @@ function Format() { // console.log("next___  " + nextPage);
 
             data.items.forEach((curr => {
                 const info = {
-                 vidTitle: curr.snippet.title,
-                 vidUrl: 'https://www.youtube.com/watch?v=' + curr.snippet.resourceId.videoId,
-                 vidThumbnail: curr.snippet.thumbnails.default.url,
-                 date: curr.snippet.publishedAt,
+                    vidTitle: curr.snippet.title,
+                    vidUrl: 'https://www.youtube.com/watch?v=' + curr.snippet.resourceId.videoId,
+                    vidThumbnail: curr.snippet.thumbnails.default.url,
+                    date: curr.snippet.publishedAt,
 
-              };
-                var markup = `<p className='vidcounter'>${count+1}</p><div key=${count} class='videoitem'><a class='vidlink' href='${info.vidUrl}' target='_blank'><img class='vidimg' src='${info.vidThumbnail}'></img></a><p>${info.vidTitle}</p></div>`;
+                };
+                var markup = `<p className='vidcounter'>${count + 1}</p><div key=${count} class='videoitem'><a class='vidlink' href='${info.vidUrl}' target='_blank'><img class='vidimg' src='${info.vidThumbnail}'></img></a><p>${info.vidTitle}</p></div>`;
                 array.push(info);
                 // document.querySelector('vid').insertAdjacentHTML('beforeend', markup);
-                
+
                 setCount(array.length);
                 // return info;
             }))
         }); //end of then(data)
     };
-    console.log(array);
+    // console.log(array);
 
-    const pushIt = array.map((vid,idx) => 
-                            <li ><p className='vidcounter'>{idx}</p><
-                            div key={idx} className='videoitem'>
-                            <a className='vidlink' href={vid.vidUrl} target='_blank'>
-                            <img classNaME='vidimg' src={vid.vidThumbnail}></img>
-                            </a>
-                            <p>{vid.vidTitle}</p>
-                            </div></li>
-                          );
 
-    return( < > < div className = "App" > < form onSubmit = {
-            submitHandler
-        } > < input ref = {
-            inputRef
-        }
-        /> < button type = "submit" > Submit < /button > < /form> < p > Submit Value: < b > {
-        newUrl
-    } < /b></p > < br / > < /div> < h2 onClick = {
-    () => getVideosPage(newUrl)
-} > click < /h2> < p key = "totalVids" > Number of videos loaded: {
-vidCount
-} < /p>
-<ul className='videolist'>{pushIt}</ul>  
-< / > 
-)
+    const pushIt = array.map((vid, idx) =>
+        <li title={vid.vidTitle} className='vidlist' ><p className='vidcounter'>{idx}</p><
+            div key={idx} className='videoitem'>
+            <a className='vidlink' href={vid.vidUrl} target='_blank'>
+                <img className='vidimg' src={vid.vidThumbnail}></img>
+            </a>
+            <p>{vid.vidTitle}</p>
+        </div></li>
+    );
+    
+    const [displayState, setDisplayState] = useState('visible');
+  
+    const handleOnInputChange = (event) => {
+        console.log(searchVal.current.value);
+        const query = !(searchVal.current.value.length == 0) ? searchVal.current.value : 'empty';
+        console.log(query);
+        // let vidTags = React.Children.toArray(elements.children).filter((item) => item.props.className.includes(query));
+        var elements = document.getElementsByTagName('li');
+        var eleArray = Array.prototype.slice.call(elements);
+        // elements.style.display = "none";
+        // console.log(eleArray);
+        eleArray.filter((item) => {
+            console.log(!item.title.includes(query));
+            if (!item.title) return item;
+            if (item.getAttribute('title').toLowerCase().includes(query.toLowerCase()) == false) {
+                // console.log( !item.getAttribute('title').toLowerCase().includes(query.toLowerCase()) === true );
+                item.style.display = "none";
+            
+                console.log('i');
+            } else {
+                item.style.display = "initial";
+            }
+            
+        });
+        // console.log(vidTags);
+        console.log(searchVal.current.value);
+
+        
+        };    
+
+
+return ( 
+    <>
+     <div className="App">
+        <form onSubmit={submitHandler}>
+        <input ref={
+        inputRef
+    }/>
+    <button type="submit" > Submit </button > </form>
+    <p>Submit Value: <b>{newUrl}</b></p>
+        <br/>
+        </div>
+        
+        <input
+   type="text"
+   ref={searchVal}
+   id="search-input"
+   placeholder="Search..."/>
+    <button type="submit"onClick={handleOnInputChange} > Submit </button >
+        <h2 onClick={() => getVideosPage(newUrl)}>click</h2>
+        <p key="totalVids">Number of videos loaded: {vidCount}</p>
+        <ul className='videolist'>{pushIt}</ul>
+     </>
+     )
 
 };
 
-function Getter() {
-    // PLg-QWgQ907Hoz61oESylThb2MK9vPC6rk
-  // PLK5U0tyd34tCTqZI9deO4VjxhgJDhhBpC
-}
-export default Format;
+                    export default Format;
